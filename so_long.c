eef2214b17cef1910debc3e9ddad37ce39ecedde
mohamedMQ <maqbour.moh@gmail.com>
2023-03-11 14:31:45 +0100

    add: map file reader, data structs and update header

---
+++ so_long.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaqbour <mmaqbour@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/08 11:11:49 by mmaqbour          #+#    #+#             */
+/*   Updated: 2023/04/08 11:11:51 by mmaqbour         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+int	len_line(char *buffer, int *chec)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (buffer[i] != '\0')
+	{
+		if (buffer[i] != '\n' && *chec == 0)
+			j++;
+		else
+			*chec = 1;
+		i++;
+	}
+	return (j);
+}
+
+int	count_of_line(char *buffer)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (buffer[i] != '\0')
+	{
+		if (buffer[i] == '\n')
+			j++;
+		i++;
+	}
+	return (j);
+}
+
+t_data	get_wid_hei(t_data data, char *argv)
+{
+	int			fd;
+	int			r;
+	int			chec;
+	static int	valid;
+	char		buffer[2];
+
+	r = 1;
+	chec = 0;
+	fd = open(argv, O_RDWR);
+	if (fd < 0)
+		exit (0);
+	while (r)
+	{
+		r = read(fd, buffer, 1);
+		if (r == -1)
+			exit (1);
+		buffer[r] = '\0';
+		data.m_p.x += len_line(buffer, &chec);
+		data.m_p.y += count_of_line(buffer);
+		valid++;
+	}
+	if (valid > 1)
+		data.m_p.y += 1;
+	close(fd);
+	return (data);
+}
+
+int	main(int argc, char *argv[])
+{
+	t_data	data;
+
+	if (argc != 2)
+		return (1);
+	initialize_some_struct(&data);
+	check_name_map(argv[1]);
+	data = get_wid_hei(data, argv[1]);
+	data = get_map_ptr(data, argv[1]);
+	check_map(&data);
+	check_path(&data, argv[1]);
+	return (0);
+}
--- so_long.h
+++ so_long.h
@@ -13,6 +13,8 @@
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+# include "./utils_functions/get_next_line.h"
+# include "./ft_printf/ft_printf.h"
 # include <fcntl.h>
 # include <stdlib.h>
 # include <unistd.h>
@@ -25,15 +27,30 @@ typedef struct map
 	int			y;
 }				t_map;
 
+typedef struct t_data_2
+{
+	char		*img_floor;
+	void		*mlx;
+	int			sum_coin;
+	int			sum_in_map;
+}				t_data_2;
+
 typedef struct s_data
 {
+	t_data_2	data_2;
 	t_map		m_p;
 	int			width;
 	int			height;
+	int			check;
+	int			just_one_coin;
 }				t_data;
 
+t_data				get_map_ptr(t_data data, char *argv);
+void				check_map(t_data *data);
+void				check_path(t_data *data, char *argv);
 void				check_name_map(char *argv);
 void				initialize_some_struct(t_data *data);
 void				eroor_map(t_data *data);
+char				*ft_itoa(int n);
 
 #endif
