92d04b5416758a4839c9c923f8cdd4ae56c472cb
mohamedMQ <maqbour.moh@gmail.com>
2023-03-30 10:29:44 +0000

    add: bonus map parsing, validation and test maps

---
+++ so_long_bonus/so_long_utils_bonus.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long_utils_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaqbour <mmaqbour@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/08 11:10:29 by mmaqbour          #+#    #+#             */
+/*   Updated: 2023/04/08 11:17:10 by mmaqbour         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long_bonus.h"
+
+void	check_name_map(char *argv)
+{
+	int			i;
+	int			j;
+	int			tmp;
+	char		*ptr;
+	static int	valid;
+
+	ptr = ".ber";
+	i = -1;
+	while (argv[++i])
+	{
+		j = 0;
+		tmp = i;
+		while (argv[tmp] == ptr[j] && ptr[j])
+		{
+			tmp++;
+			j++;
+		}
+		if (ptr[j] == '\0')
+			valid = 1;
+	}
+	if (valid == 0)
+	{
+		ft_printf("Name map invalid !!!\n");
+		exit(1);
+	}
+}
+
+void	initialize_some_struct(t_data *data)
+{
+	data->just_one_coin = 0;
+	data->data_2.sum_coin = 0;
+	data->check = 0;
+	data->m_p.y = 0;
+	data->m_p.x = 0;
+	data->data_2.check_lose = 0;
+	data->m_p.ptr = 0;
+	data->m_p.ptr_2 = 0;
+}
+
+void	eroor_map(t_data *data)
+{
+	if (data->m_p.ptr)
+		free(data->m_p.ptr);
+	if (data->m_p.ptr_2)
+		free(data->m_p.ptr_2);
+	ft_printf("Error\n");
+	ft_printf("you have error in your map\n");
+	exit(1);
+}
+
+t_data	get_map_ptr(t_data data, char *argv)
+{
+	int	i;
+	int	fd;
+
+	i = 0;
+	data.m_p.ptr = (char **)malloc((data.m_p.y + 1) * sizeof(char *));
+	if (!data.m_p.ptr)
+		exit (1);
+	fd = open(argv, O_RDWR);
+	if (fd < 0)
+	{
+		ft_printf("Check your .ber file !!!\n");
+		exit (0);
+	}
+	while (i < data.m_p.y)
+		data.m_p.ptr[i++] = get_next_line(fd);
+	data.m_p.ptr[data.m_p.y] = 0;
+	close(fd);
+	return (data);
+}
